0bc58c88ba2c3a7287d0e023a5ae385f86db5d95
BrundaMBharadwaj <brundabharadwaj22@gmail.com>
2022-10-12 19:14:01 +0530

    adding delete and add button

--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
 const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return(
-                <div className="collection-list center" key={todo.id}>
+                <div className="collection-list center" key={todo.id} style={{paddingBottom:'2%'}}>
                     <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+                    <Button onClick={()=>{deleteTodo(todo.id)}} variant='filled' style={{backgroundColor:'#e3415f',color:'white', float:'right', padding:'0%'}}>-</Button>
                 </div>
             )
         })
@@ -13,7 +14,7 @@ const Todos = ({todos, deleteTodo}) => {
         <p className="center">You have no todos left</p>
     )
     return(
-        <div className="todos collection">
+        <div className="todos collection" style={{padding:'2%',borderRadius:'2%'}}>
             {todoList}
         </div>
     )
--- src/addTodo.js
+++ src/addTodo.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+import Button from '@mui/material/Button';
 class AddTodo extends Component {
     state = {
-        content: 'Enter todo task here'
+        content: ''
     }
     handleChange = (e) => {
         this.setState({
             content: e.target.value
         })
+    }
+    addToList=()=>{
+ 
+var element= document.getElementById('input').value
+console.log(element)
+this.props.addTodo(this.state)
+this.setState({content:''})
     }
     hadleSubmit = (e) => {
         e.preventDefault();
@@ -20,8 +27,8 @@ class AddTodo extends Component {
         return(
             <div>
                 <form onSubmit={ this.hadleSubmit }>
-                    <label>Add todo</label>
-                    <input type="text" onChange={ this.handleChange } value={this.state.content}></input>
+                 <input type="text" id='input' className='toDo-input'style={{width:'92%',height:'20%', marginRight:'8px'}} onChange={ this.handleChange } value={this.state.content}></input>
+                 <Button  variant="contained" onClick={this.addToList}>+</Button>
                 </form>
             </div>
         )
